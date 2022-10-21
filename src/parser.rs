59397103836c7891d9294fbc6754e6dd74ea4326
Olly Legg <olly@51degrees.net>
2022-10-21 19:34:27 +0100

    Iterate file, and verify correct number of shots, trips, and references

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,30 @@
+use nom::{
+	multi::count,
+	number::complete::{le_i16, le_i32, le_i64, le_u16, le_u32, le_u8},
+	IResult,
+};
 use thiserror::Error;
+
+#[derive(Debug)]
+pub struct Document {
+	references: Box<[()]>,
+	shots: Box<[()]>,
+	trips: Box<[()]>,
+}
+
+impl Document {
+	pub fn references(&self) -> &[()] {
+		&self.references
+	}
+
+	pub fn shots(&self) -> &[()] {
+		&self.shots
+	}
+
+	pub fn trips(&self) -> &[()] {
+		&self.trips
+	}
+}
 
 #[derive(Debug, Error, Eq, PartialEq)]
 pub enum ParserError<'a> {
@@ -7,12 +33,15 @@ pub enum ParserError<'a> {
 
 	#[error("unsupported version: {0}")]
 	UnsupportedVersion(u8),
+
+	#[error("unknown error")]
+	UnknownError,
 }
 
 static HEADER: [u8; 3] = [b'T', b'o', b'p'];
 static VERSION: u8 = 0x3;
 
-pub fn parse<'a>(contents: &'a [u8]) -> Result<(), ParserError<'a>> {
+pub fn parse(contents: &[u8]) -> Result<Document, ParserError> {
 	let header = &contents[0..3];
 	if header != HEADER {
 		return Err(ParserError::InvalidHeader(header));
@@ -23,7 +52,116 @@ pub fn parse<'a>(contents: &'a [u8]) -> Result<(), ParserError<'a>> {
 		return Err(ParserError::UnsupportedVersion(version));
 	}
 
-	Ok(())
+	// TODO: remove unwrap
+	Ok(parse_internal(&contents[4..]).unwrap().1)
+}
+
+fn parse_internal(input: &[u8]) -> IResult<&[u8], Document> {
+	let (input, trips) = parse_trips(input)?;
+	let (input, shots) = parse_shots(input)?;
+	let (input, references) = parse_references(input)?;
+
+	Ok((
+		input,
+		Document {
+			references: references.into_boxed_slice(),
+			shots: shots.into_boxed_slice(),
+			trips: trips.into_boxed_slice(),
+		},
+	))
+}
+
+fn parse_shots(input: &[u8]) -> IResult<&[u8], Vec<()>> {
+	// length_count(le_u32, parse_shot)(input)
+
+	let length: IResult<&[u8], u32> = le_u32(input);
+	let (input, length): (_, u32) = length.unwrap_or((input, 0));
+	count(parse_shot, length as usize)(input)
+}
+
+// Shot = {
+//   Id from
+// 	 Id to
+// 	 Int32 dist  // mm
+// 	 Int16 azimuth  // internal angle units (full circle = 2^16, north = 0, east = 0x4000)
+// 	 Int16 inclination // internal angle units (full circle = 2^16, up = 0x4000, down = 0xC000)
+// 	 Byte flags  // bit0: flipped shot
+// 	 Byte roll   // roll angle (full circle = 256, disply up = 0, left = 64, down = 128)
+// 	 Int16 tripIndex  // -1: no trip, >=0: trip reference
+// 	 if (flags & 2)
+// 	   String comment
+// }
+fn parse_shot(input: &[u8]) -> IResult<&[u8], ()> {
+	let (input, _from) = le_i32(input)?;
+	let (input, _to) = le_i32(input)?;
+	let (input, _distance) = le_i32(input)?;
+	let (input, _azimuth) = le_i16(input)?;
+	let (input, _inclination) = le_i16(input)?;
+	let (input, _flags) = le_u8(input)?;
+	let (input, _roll) = le_u8(input)?;
+	let (input, _trip_index) = le_u16(input)?;
+
+	// TODO: parse conditional comment
+
+	Ok((input, ()))
+}
+
+// String = { // .Net string format
+//   Byte[] length // unsigned, encoded in 7 bit chunks, little endian, bit7 set in all but the last byte
+//   Byte[length]  // UTF8 encoded, 1 to 3 bytes per character, not 0 terminated
+// }
+fn parse_string(input: &[u8]) -> IResult<&[u8], ()> {
+	// TODO: implement
+	let (input, length) = le_u8(input)?;
+	debug_assert_eq!(length, 0);
+
+	Ok((input, ()))
+}
+
+fn parse_references(input: &[u8]) -> IResult<&[u8], Vec<()>> {
+	// length_count(le_u32, parse_reference)(input)
+
+	let length: IResult<&[u8], u32> = le_u32(input);
+	let (input, length): (_, u32) = length.unwrap_or((input, 0));
+	count(parse_reference, length as usize)(input)
+}
+
+// Reference = {
+//   Id station
+// 	 Int64 east     // mm
+// 	 Int64 north    // mm
+// 	 Int32 altitude // mm above sea level
+// 	 String comment
+// }
+fn parse_reference(input: &[u8]) -> IResult<&[u8], ()> {
+	let (input, _station) = le_i32(input)?;
+	let (input, _east) = le_i64(input)?;
+	let (input, _north) = le_i64(input)?;
+	let (input, _altitude) = le_i32(input)?;
+	let (input, _comment) = parse_string(input)?;
+
+	Ok((input, ()))
+}
+
+fn parse_trips(input: &[u8]) -> IResult<&[u8], Vec<()>> {
+	// length_count(le_u32, parse_trip)(input)
+
+	let length: IResult<&[u8], u32> = le_u32(input);
+	let (input, length): (_, u32) = length.unwrap_or((input, 0));
+	count(parse_trip, length as usize)(input)
+}
+
+// Trip = {
+//   Int64 time  // ticks (100ns units starting at 1.1.1)
+// 	 String comment
+// 	 Int16 declination  // internal angle units (full circle = 2^16)
+// }
+fn parse_trip(input: &[u8]) -> IResult<&[u8], ()> {
+	let (input, _time) = le_i64(input)?;
+	let (input, _comment) = parse_string(input)?;
+	let (input, _declination) = le_i16(input)?;
+
+	Ok((input, ()))
 }
 
 #[cfg(test)]
---
+++ tests/parser_test.rs
@@ -0,0 +1,34 @@
+use std::{fs::File, io::Read, path::PathBuf};
+
+use pocket_topo::parser;
+
+#[test]
+fn parses_default() {
+	let contents = fixture("default.top");
+
+	let document = parser::parse(&contents).expect("invalid document");
+
+	let mut references = document.references().iter();
+	assert_eq!(references.len(), 1);
+
+	let _reference = references.next().expect("expected reference");
+
+	assert!(references.next().is_none());
+
+	assert_eq!(document.trips().len(), 0);
+
+	assert_eq!(document.shots().len(), 1);
+}
+
+fn fixture(fixture: &str) -> Vec<u8> {
+	let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+	path.push("tests/fixtures");
+	path.push(fixture);
+
+	let mut file = File::open(path).unwrap();
+
+	let mut buffer = Vec::new();
+	file.read_to_end(&mut buffer).unwrap();
+
+	buffer
+}
