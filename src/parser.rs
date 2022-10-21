834e388a19a2ad5cd67dd203e685a565c356c5c5
Olly Legg <olly@51degrees.net>
2022-10-21 16:31:38 +0100

    Check file version number

--- src/parser.rs
+++ src/parser.rs
@@ -4,17 +4,26 @@ use thiserror::Error;
 pub enum ParserError<'a> {
 	#[error("invalid header: {0:?}")]
 	InvalidHeader(&'a [u8]),
+
+	#[error("unsupported version: {0}")]
+	UnsupportedVersion(u8),
 }
 
 static HEADER: [u8; 3] = [b'T', b'o', b'p'];
+static VERSION: u8 = 0x3;
 
 pub fn parse<'a>(contents: &'a [u8]) -> Result<(), ParserError<'a>> {
 	let header = &contents[0..3];
-	if header == HEADER {
-		Ok(())
-	} else {
+	if header != HEADER {
 		return Err(ParserError::InvalidHeader(header));
 	}
+
+	let version = contents[3];
+	if version != VERSION {
+		return Err(ParserError::UnsupportedVersion(version));
+	}
+
+	Ok(())
 }
 
 #[cfg(test)]
@@ -37,5 +46,16 @@ mod test {
 		assert_eq!(error, ParserError::InvalidHeader(&[b'T', b'O', b'P']));
 
 		assert_eq!(error.to_string(), "invalid header: [84, 79, 80]");
+	}
+
+	#[test]
+	fn test_invalid_version() {
+		let contents = vec![b'T', b'o', b'p', 0x2];
+		let result = parse(&contents);
+
+		let error = result.expect_err("expected `ParserError`");
+		assert_eq!(error, ParserError::UnsupportedVersion(0x2));
+
+		assert_eq!(error.to_string(), "unsupported version: 2");
 	}
 }
